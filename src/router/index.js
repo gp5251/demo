cfa020ed6ae5f1cfe155ca84b93eed005c8003b9
lijiwei <lijiwei@daojia-inc.com>
2018-08-22 21:01:29 +0800

    add jsonp

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Jsonp from '@/components/Jsonp/index'
+
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
-   
+	   {
+	   	 	path: '/jsonp',
+	   	 	name: 'jsonp',
+	   	 	component: Jsonp
+	   }
   ]
 })
