3847f41c21d7715145ce5a6d41fd71bdbaacc6ce
lijiwei <lijiwei@daojia-inc.com>
2018-11-12 16:15:57 +0800

    clear all

--- src/js/nodejs/静态web服务器/service_cb.js
+++
@@ -1,8 +0,0 @@
-const http =  require("http")
-const { statics } = require("./router/router")
-
-http.createServer((req, res) => {
-   
-    statics(req, res, __dirname);
-    
-}).listen(8088, "localhost")
--- todo.md
+++
@@ -1,9 +0,0 @@
-1. 跨域
-
-2. npm jsonp
-
-3. router_blank
-
-4. javascript文件读取，上传（文件、图片、视频等）
-
-4. 雪碧图制作
