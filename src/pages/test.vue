4ab3cc2d9381163f4e2995a2a67661d191236c55
lijiwei <lijiwei@daojia-inc.com>
2018-11-28 17:36:26 +0800

    update deepclone

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,95 +1,107 @@
 <template>
-    <div class="test">
-
-        <button @click="showToast">showToast</button>
-        <button @click="showloading">showloading</button>
-
-    </div>
+  <div class="test">
+    <button @click="showToast">showToast</button>
+    <button @click="showloading">showloading</button>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+import addParams from "@/utils/addParams";
+import deepClone from "@/utils/deepClone";
+import jsonp from "@/utils/jsonp";
+import need from "@/utils/need";
+import queryString from "@/utils/queryString";
+import setRem from "@/utils/setRem";
+import Toast from "@/plugins/toast";
+import loading from "@/plugins/loading";
+import request from "@/plugins/request";
 
-import Vue from "vue"
-import addParams from "@/utils/addParams"
-import deepClone from "@/utils/deepClone"
-import jsonp from "@/utils/jsonp"
-import need from "@/utils/need"
-import queryString from "@/utils/queryString"
-import setRem from "@/utils/setRem"
-import Toast from "@/plugins/toast"
-import loading from "@/plugins/loading"
-import request from "@/plugins/request"
-
-Vue.use(Toast)
-Vue.use(loading)
-Vue.use(request)
+Vue.use(Toast);
+Vue.use(loading);
+Vue.use(request);
 
 export default {
-    name: "test",
-    methods: {
-        showToast () {
-            this.$toast({
-                message: "hello"
-            })
-        },
-        showloading () {
-            const loading = this.$loading()
-            setTimeout(() => {
-                loading.close()
-                console.log("timeout")
-            }, 3000);
-        }
+  name: "test",
+  methods: {
+    showToast() {
+      this.$toast({
+        message: "hello"
+      });
     },
-    mounted () {
-        // console.log(addParams("www.aaa.com?c=3", {a:2,b:9}))
-        // let a = {
-        //     a: 3,
-        //     b: {c: 4}
-        // }
-
-        // let b = deepClone(a)
-        // b.b.c = 5
-        // console.log(a)
-        // console.log(b)
-
-        // let arr1 = [
-        //     "a",
-        //     {b: 3}
-        // ]
-
-        // let arr2 = deepClone(arr1)
-        // // let arr2 = arr1
-        // arr2[1].b= 8
-        // console.log(arr1)
-        // console.log(arr2)
-        console.log("hahah")
-        const url = "https://open-anzhuang-betaa.djtest.cn/index/getDemandDetail"
-        const data = {
-            demandStatus: 5,
-            pageNum: 1,
-            countEachPage: 10
-        }
-
-        jsonp(url, data).then(res => {
-            console.log(res)
-        })
-
-        need("https://static.daojia.com/bi/buried_point/js/tracker.js", function () {
-            console.log("need success")
-        })
-
-        // setRem ()
-
-        this.$request({
-            url: "https://plugins.kancloud.cn/api/plugin/info?name=theme-default,highlight"
-        }).then(res => {
-            console.log(res)
-        })
-
+    showloading() {
+      const loading = this.$loading();
+      setTimeout(() => {
+        loading.close();
+        console.log("timeout");
+      }, 3000);
     }
-}
+  },
+  mounted() {
+    // console.log(addParams("www.aaa.com?c=3", {a:2,b:9}))
+    // let a = {
+    //     a: 3,
+    //     b: {c: 4}
+    // }
+
+    // let b = deepClone(a)
+    // b.b.c = 5
+    // console.log(a)
+    // console.log(b)
+
+    // let arr1 = [
+    //     "a",
+    //     {b: 3}
+    // ]
+
+    // let arr2 = deepClone(arr1)
+    // // let arr2 = arr1
+    // arr2[1].b= 8
+    // console.log(arr1)
+    // console.log(arr2)
+    var d = {
+      a: "aaa",
+      b() {
+        console.log(this);
+      },
+      c: {
+        d: 8
+      },
+      f: new Date(),
+      g: /a/i
+    };
+
+    var e = deepClone(d);
+    e.c.d = 6;
+    d.b();
+    e.b();
+    console.log("hahah");
+    const url = "https://open-anzhuang-betaa.djtest.cn/index/getDemandDetail";
+    const data = {
+      demandStatus: 5,
+      pageNum: 1,
+      countEachPage: 10
+    };
+
+    jsonp(url, data).then(res => {
+      console.log(res);
+    });
+
+    need("https://static.daojia.com/bi/buried_point/js/tracker.js", function() {
+      console.log("need success");
+    });
+
+    // setRem ()
+
+    this.$request({
+      url:
+        "https://plugins.kancloud.cn/api/plugin/info?name=theme-default,highlight"
+    }).then(res => {
+      console.log(res);
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/utils/deepClone.js
+++ src/utils/deepClone.js
@@ -16,22 +16,75 @@
 //     return result
 // }
 
-export default function deepClone (obj) {
-    if (obj instanceof Object) {
+// export default function deepCloneCommon (obj) {
+//     if (obj instanceof Object) {
 
-        let result = obj instanceof Array ? [] : {}
+//         let result = obj instanceof Array ? [] : {}
 
-        for (let key in obj) {
+//         for (let key in obj) {
 
-            result[key] = obj[key] instanceof Object ? deepClone(obj[key]) : obj[key]
+//             result[key] = obj[key] instanceof Object ? deepClone(obj[key]) : obj[key]
 
+//         }
+
+//         return result
+
+//     } else {
+
+//         return obj
+
+//     }
+// }
+
+export default function deepClone (obj, parent = {}) {
+    if (typeof obj == "object" || typeof obj == "function") {
+        let Constructor = obj.constructor
+        let cloneObj
+        switch (Constructor) {
+            case RegExp:
+            case Date: 
+                cloneObj = new Constructor (obj);
+                break;
+            // case Date: 
+            //     cloneObj = new Constructor (obj);
+            //     break;
+            case Function:
+                cloneObj = obj.bind(parent);
+                break;
+            default: 
+                cloneObj = new Constructor()
+                break;
         }
 
-        return result
+        for (let key in obj) {
+            cloneObj[key] = deepClone (obj[key], cloneObj)
+        }
+
+        return cloneObj
 
     } else {
-
         return obj
-
     }
 }
+
+// var d = {
+//     a: "aaa",
+//     b () {
+//         console.log(this)
+//     },
+//     c: {
+//         d: 8
+//     },
+//     f: new Date(),
+//     g: /a/i
+// }
+
+// var e = deepClone(d)
+// e.c.d = 6
+// d.b()
+// e.b()
+// console.log(d.c.d)
+// console.log(e.c.d)
+
+// console.log(d.valueOf())
+// console.log(e.valueOf())
