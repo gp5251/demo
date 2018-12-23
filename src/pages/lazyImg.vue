89b864783c7c280b9bfe89d3f47a34535cb72619
lijiwei <1101407798@qq.com>
2018-12-23 09:23:13 +0800

    lazyload update

--- src/pages/lazyImg.vue
+++ src/pages/lazyImg.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="lazyImg">
+    <div class="lazyImg" ref="lazyImg">
 
         <img class="img" :src="defaultSrc" alt="" :data-src="src" v-for="(src, index) in imgList" :key="index" ref="imgs">
     </div>
@@ -10,21 +10,21 @@ export default {
     name: "lazyImg",
     data() {
         return {
-            defaultSrc: "http://img2.imgtn.bdimg.com/it/u=764856423,3994964277&fm=26&gp=0.jpg",
+            defaultSrc: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545538035116&di=689f08c16c2e4df2faff57fd193fae9d&imgtype=0&src=http%3A%2F%2Ftx.haiqq.com%2Fuploads%2Fallimg%2F170507%2F0H1232460-0.jpg",
             imgList:[
                 "http://img2.imgtn.bdimg.com/it/u=764856423,3994964277&fm=26&gp=0.jpg",
-                "http://pic28.photophoto.cn/20130818/0020033143720852_b.jpg",
-                "http://pic19.nipic.com/20120210/7827303_221233267358_2.jpg",
-                "http://pic33.photophoto.cn/20141022/0019032438899352_b.jpg",
+                "http://img17.3lian.com/d/file/201703/13/4758dd3347da01d7e082825fa8b0b0cc.jpg",
+                "http://img18.3lian.com/d/file/201711/11/aff81832b1472c2a44f2c703a4c0236c.png",
+                "http://img5.duitang.com/uploads/item/201508/30/20150830235219_BHiaC.thumb.700_0.jpeg",
                 "http://img2.imgtn.bdimg.com/it/u=2569486047,3428433554&fm=214&gp=0.jpg",
                 "http://img.zcool.cn/community/0188915805cc66a84a0e282b933a57.jpg@1280w_1l_2o_100sh.png",
                 "http://img.zcool.cn/community/0151b15764b1060000018c1b6060d6.jpg@900w_1l_2o_100sh.jpg",
-                "http://pic29.photophoto.cn/20131011/0006019796700187_b.jpg",
+                "http://img18.3lian.com/d/file/201709/18/59b79985b824db2aa4ea5f5ba898835d.jpg",
                 "http://txt25-2.book118.com/2017/0922/book134724/134723007.jpg",
-                "http://pic36.nipic.com/20131213/6704106_223232205107_2.png",
+                "http://photo.16pic.com/00/09/15/16pic_915841_b.jpg",
                 "http://img.zcool.cn/community/01f5795541d50b00000115410b205a.jpg",
                 "http://www.ctps.cn/PhotoNet/Profiles2011/20160311/20163111610178319.jpg",
-                "http://pic38.nipic.com/20140212/17942401_101320663138_2.jpg",
+                "http://image.biaobaiju.com/uploads/20180803/21/1533302926-DlFZTdtQGb.jpg",
             ],
             curImg: 0
         }
@@ -36,7 +36,13 @@ export default {
         let lazy = this.lazyLoad()
         // lazy()
         window.addEventListener( "scroll", this.throttle(lazy, 500, 1000), false )
-        
+        // this.$nextTick(lazy)
+        // setTimeout(lazy, 3000);
+        // window.onload = lazy
+        let img = new Image()
+        img.onload = lazy
+        img.src = this.defaultSrc
+        // this.$refs.lazyImg.onload = lazy
     },
 
     methods: {
@@ -69,7 +75,7 @@ export default {
                 for ( let i = curImg; i < len; i++ ) {
                     if (allImg[i].offsetTop < clientHeight + scrollTop) {
                         console.log("allImg[" + i + "].offsetTop " + allImg[i].offsetTop)
-                        if (allImg[i].getAttribute("src") == _this.imgList[0]) {
+                        if (allImg[i].getAttribute("src") == _this.defaultSrc) {
                             allImg[i].src = allImg[i].getAttribute("data-src")
                         }
                         curImg += 1
