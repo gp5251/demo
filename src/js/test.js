b716b6c4c63d4d7333b9b2d2a12ecf6b5725f3f7
lijiwei <1101407798@qq.com>
2019-02-23 14:07:09 +0800

    call bind apply new 防抖 节流

---
+++ src/js/test.js
@@ -0,0 +1,17 @@
+Function.prototype.myfn = function (context) {
+    // console.log(this)
+    context.fn = this
+    context.fn()
+}
+
+let a = {
+    fn () {
+        console.log(this)
+    }
+}
+
+let b = {
+    name: "b"
+}
+
+a.fn.myfn (b)
